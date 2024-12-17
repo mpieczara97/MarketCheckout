404d17bf483497c08b40db8f075612208e7f8e34
MichalPieczra <michal.piecara97@gmail.com>
2024-12-17 22:29:45 +0100

    Initial commit

---
+++ src/main/java/com/example/MarketCheckout/MarketCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarketCheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketCheckoutApplication.class, args);
+	}
+
+}
